7a7ab8afbddfa083ee7c982d48d882cc08901e62
Adam Russell <arussell333@gmail.com>
2016-04-25 11:34:08 +0200

    1 commit

---
+++ Iphonetests/src/com/appium/IphoneBasictest.java
@@ -0,0 +1,39 @@
+package com.appium;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
+import io.appium.java_client.ios.IOSDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+
+public class IphoneBasictest {
+	
+	public static void main(String[] args) throws MalformedURLException, InterruptedException{
+		
+		
+		DesiredCapabilities capabilities=new DesiredCapabilities();
+		capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+		capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.IOS);
+		capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9.0");
+		capabilities.setCapability(MobileCapabilityType.BROWSER_NAME,"safari");
+		capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone Simulator");
+		IOSDriver driver=new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+		driver.get("https://www.google.ie");
+		Thread.sleep(4000);
+		driver.quit();
+		
+		
+		
+		
+		
+		
+	}
+
+}
